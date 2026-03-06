a9cb7f44ce74c15e63ae52832e3ca51a89edcb13
dionlucas10 <dion.lucas1012@hotmail.com>
2026-03-05 22:33:55 -0300

    finalizado o projeto pai

---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
+import { ContatoService } from '../../services/contato.service';
+import { RouterLink } from '@angular/router';
+import { Contato } from '../../componentes/contato/contato';
+import { PerfilContatoComponent } from '../perfil-contato/perfil-contato.component';
+
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+    FormularioContatoComponent,
+    RouterLink,
+    PerfilContatoComponent
+  ],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css',
+})
+export class ListaContatosComponent implements OnInit {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = [];
+
+  filtroPorTexto: string = '';
+
+  constructor(private contatoService: ContatoService) {}
+
+  ngOnInit() {
+  this.contatoService.obterContatos().subscribe(listaContatos =>{
+    this.contatos = listaContatos;
+  });
+  }
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter((contato) => {
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter((contato) => {
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .startsWith(this.removerAcentos(letra).toLowerCase());
+    });
+  }
+}
